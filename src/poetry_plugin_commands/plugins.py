161474f001d2986b89cfffdc3801214c2965ee47
Asad Sayfiyev <bcdev@mail.ru>
2024-04-08 07:42:57 +0630

    Update plugins.py

--- src/poetry_plugin_commands/plugins.py
+++ src/poetry_plugin_commands/plugins.py
@@ -28,6 +28,7 @@ class UserCommandsApplicationPlugin(ApplicationPlugin):
 		toml_content: dict[str, Any] = self.poetry.pyproject.data
 
 		# TODO: Exec field or catch options of poetry's run command (mb better make something like crun?)
+		# TODO: Make profiles for ux..
 		user_commands = toml_content['tool']['poetry']['plugins'][self.plugin_section]
 
 		# TODO: Support python commands using toml's optioned values..
